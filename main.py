5b53e89c378d08e71eb7264db236aefd5ba82619
Colby Ryan <colbyr.ryan@gmail.com>
2022-02-14 11:42:16 -0600

    working on bug with colors

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import turtle
+import time
+import random
 
 WIDTH, HEIGHT = 500, 500
-
+COLORS = ['red', 'green', 'blue', 'orange', 'pink', 'purple', 'cyan', 'black', 'brown']
 
 
 def get_number_of_racers():
@@ -18,10 +20,43 @@ def get_number_of_racers():
         else:
             print('Input is not in range (2-10) try again')
 
+def race(colors):
+   turtles = create_turtles(colors)
+
+   while True:
+       for racer in turtles:
+           distance = random.randrange(1, 20)
+           racer.forward(distance)
+
+           x, y = racer.pos()
+           if y >= HEIGHT // 2 - 10:
+               return colors[turtles.index(racer)]
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+
+    return turtles
+
 def init_turtle():
     screen = turtle.Screen()
     screen.setup(WIDTH, HEIGHT)
     screen.title('Turtle Racing')
 
 racers = get_number_of_racers()
-print(racers)
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner = race(colors)
+print(winner)
